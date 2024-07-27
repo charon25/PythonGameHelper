b2194321f53f9c4f8cd2e243511127cd262dc544
Paul Kern <paul.kern.fr@gmail.com>
2024-07-27 11:57:32 +0200

    Add Scale object and related functions

--- pyghelper/__init__.py
+++ pyghelper/__init__.py
@@ -2,4 +2,4 @@ from pyghelper.animation_manager import Animation, AnimationManager
 from pyghelper.event_manager import EventManager
 from pyghelper.images import Image, Sprite
 from pyghelper.sound_manager import SoundManager
-from pyghelper.utils import Window
+from pyghelper.utils import Window, Scale
